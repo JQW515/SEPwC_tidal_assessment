d9fbfecd0ed166485b804ffab7e9295ca53831ae
Toby Read <jqw515@york.ac.uk>
2026-05-07 18:04:50 +0100

    Replacing H for h

--- test/get_longest_contiguous_data_function_test.py
+++ test/get_longest_contiguous_data_function_test.py
@@ -5,9 +5,9 @@ from tidal_analysis import get_longest_contiguous_data
 
 def test_longest_contiguous_logic():
     # Creating dummy data with a gap in
-    times = pd.date_range("2023-01-01", periods=10, freq="H")
+    times = pd.date_range("2023-01-01", periods=10, freq="h")
     # Skipping 5 hours to create a gap
-    times2 = pd.date_range("2023-01-01 15:00:00", periods=20, freq="H")
+    times2 = pd.date_range("2023-01-01 15:00:00", periods=20, freq="h")
     # Merging and adding data
     combined_index = times.union(times2)
     df = pd.DataFrame({"Sea Level": np.random.rand(len(combined_index))}, index=combined_index)
