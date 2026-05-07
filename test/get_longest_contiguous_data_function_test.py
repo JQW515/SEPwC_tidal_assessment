2d50e57163dba1c6ca4356914ef60eafdff74205
Toby Read <jqw515@york.ac.uk>
2026-05-07 18:00:43 +0100

    Enveloping the test into a function

--- test/get_longest_contiguous_data_function_test.py
+++ test/get_longest_contiguous_data_function_test.py
@@ -3,15 +3,16 @@ import numpy as np
 from tidal_analysis import get_longest_contiguous_data
 
 
-
-# Creating dummy data with a gap in
-times = pd.date_range("2023-01-01", periods=10, freq="H")
-# Skipping 5 hours to create a gap
-times2 = pd.date_range("2023-01-01 15:00:00", periods=20, freq="H")
-# Merging and adding data
-combined_index = times.union(times2)
-df = pd.DataFrame({"Sea Level": np.random.rand(len(combined_index))}, index=combined_index)
-# Sending gapped data into the function
-longest = get_longest_contiguous_data(df)
-# Checking the function is correct, the second block of data is longer
-assert len(longest) == 20
+def test_longest_contiguous_logic():
+    # Creating dummy data with a gap in
+    times = pd.date_range("2023-01-01", periods=10, freq="H")
+    # Skipping 5 hours to create a gap
+    times2 = pd.date_range("2023-01-01 15:00:00", periods=20, freq="H")
+    # Merging and adding data
+    combined_index = times.union(times2)
+    df = pd.DataFrame({"Sea Level": np.random.rand(len(combined_index))}, index=combined_index)
+    # Sending gapped data into the function
+    longest = get_longest_contiguous_data(df)
+    # Checking the function is correct, the second block of data is longer
+    assert len(longest) == 20
+    
