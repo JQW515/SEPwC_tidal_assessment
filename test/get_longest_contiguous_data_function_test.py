b0ec470ddfd345661c0b672473fd7a5ca647ebae
Toby Read <jqw515@york.ac.uk>
2026-05-07 16:10:55 +0100

    Creating dummy data with a 5 hour gap

--- test/get_longest_contiguous_data_function_test.py
+++ test/get_longest_contiguous_data_function_test.py
@@ -1,3 +1,10 @@
 import pandas as pd
 import numpy as np
 from tidal_analysis import get_longest_contiguous_data
+
+
+
+# Creating dummy data with a gap in
+times = pd.date_range("2023-01-01", periods=10, freq="H")
+# Skipping 5 hours to create a gap
+times2 = pd.date_range("2023-01-01 15:00:00", periods=20, freq="H")
