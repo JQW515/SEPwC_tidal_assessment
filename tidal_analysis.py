ce1690bd502ea6146d7fe224aec46f16165f15ff
Toby Read <jqw515@york.ac.uk>
2026-05-07 12:55:31 +0100

    Assigning each block of data a unique id

--- tidal_analysis.py
+++ tidal_analysis.py
@@ -66,6 +66,7 @@ def get_longest_contiguous_data(data):
         return data
     expected_step = time_diffs.mode().iloc[0]
     gap_mask = time_diffs > expected_step
+    group_ids = gap_mask.cumsum()
     
 
     return 
