55bfbdef22cf15c6488ff74445d0e28921d7f6ac
Toby Read <jqw515@york.ac.uk>
2026-05-07 16:12:39 +0100

    Merging and adding data

--- test/get_longest_contiguous_data_function_test.py
+++ test/get_longest_contiguous_data_function_test.py
@@ -8,3 +8,6 @@ from tidal_analysis import get_longest_contiguous_data
 times = pd.date_range("2023-01-01", periods=10, freq="H")
 # Skipping 5 hours to create a gap
 times2 = pd.date_range("2023-01-01 15:00:00", periods=20, freq="H")
+# Merging and adding data
+combined_index = times.union(times2)
+df = pd.DataFrame({"Sea Level": np.random.rand(len(combined_index))}, index=combined_index)
