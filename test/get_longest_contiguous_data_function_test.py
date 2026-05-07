a06b5b6599a043e6123db87c1bdb73a5e7009adb
Toby Read <jqw515@york.ac.uk>
2026-05-07 16:28:12 +0100

    Sending gapped data into the function

--- test/get_longest_contiguous_data_function_test.py
+++ test/get_longest_contiguous_data_function_test.py
@@ -11,3 +11,5 @@ times2 = pd.date_range("2023-01-01 15:00:00", periods=20, freq="H")
 # Merging and adding data
 combined_index = times.union(times2)
 df = pd.DataFrame({"Sea Level": np.random.rand(len(combined_index))}, index=combined_index)
+# Sending gapped data into the function
+longest = get_longest_contiguous_data(df)
