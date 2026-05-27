1f205a9730b9847eac19cf2172534fdfbaf9f8c6
Toby Read <jqw515@york.ac.uk>
2026-05-27 13:59:30 +0100

    Creating fake set of data

--- test/find_extreme_tides_test.py
+++ test/find_extreme_tides_test.py
@@ -6,4 +6,13 @@ Created on Wed May 27 12:54:15 2026
 """
 
 def test_find_extreme_tides(self):
+    """ Test to verify that find_extreme_tides identifies the correct
+    maximum and minimum data"""
+    # Creating fake data sets 
+    timestamps = pd.to_datetime([
+            '2026-01-01 00:00:00',
+            '2026-01-01 01:00:00',
+            '2026-01-01 02:00:00',
+            '2026-01-01 03:00:00'
+        ])
     
